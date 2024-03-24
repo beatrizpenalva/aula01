7d2a7543c69bcc889d081076990d4604bf3d47d1
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 10:25:25 -0300

    feat(InfoRow): create component

---
+++ src/atoms/InfoRow/InfoRow.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import './InfoRow.styles.css'
+
+const InfoRow = ({ colored, label, value }) => (
+    <>
+        {Boolean(value) ? null : (<>
+            <div className={`row${colored ? ' colored' : ''}`}>
+                <p>{label}</p>
+                <p>{value}</p>
+            </div>
+            <hr aria-hidden />
+
+        </>)}
+    </>
+)
+
+InfoRow.propTypes = {
+    colored: PropTypes.bool,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+}
+
+InfoRow.defaultProps = {
+    colored: false
+}
+
+export default InfoRow
