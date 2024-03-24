b1928d563a195ba5ba6ac0a376ddbcd6fc36b4a9
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 16:15:29 -0300

    feat(Radio): crete component

---
+++ src/atoms/Radio/Radio.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import './Radio.styles.css'
+
+const Radio = ({ checked, group, id, label, onChange, value }) => (
+    <div className={`radio-input-container${checked ? ' radio-checked' : ''}`}>
+        <input type="radio" id={id} name={group} value={value} onChange={onChange} />
+        <label htmlFor={id}>{label}</label>
+    </div>
+)
+
+Radio.propTypes = {
+    checked: PropTypes.bool,
+    group: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+}
+
+Radio.defaultProps = {
+    checked: false,
+}
+
+export default Radio
