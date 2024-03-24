8bc13542a6c6bc1a2fc19cdacfebe6f73fe870cf
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 00:08:13 -0300

    feat(Button): create component

---
+++ src/atoms/Button/Button.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import './Button.styles.css'
+
+const style = {
+    primary: 'primary',
+    secondary: 'secondary',
+    warning: 'warning',
+}
+
+const Button = ({ ariaLabel, children, onClick, type, variant }) => (
+    <button aria-label={ariaLabel} className={style[variant]} onClick={onClick} type={type}>{children}</button>
+)
+
+Button.propTypes = {
+    ariaLabel: PropTypes.string,
+    children: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    type: PropTypes.oneOf(['button', 'submit']),
+    variant: PropTypes.oneOf(['primary', 'secondary', 'warning'])
+}
+
+Button.defaultProps = {
+    ariaLabel: '',
+    type: 'button',
+    variant: 'primary',
+}
+
+export default Button
