ef1ff676b483aaf6c08afd56e11309f7adafe414
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 18:59:41 -0300

    chore(RadioImage): add props validation

--- src/atoms/RadioImage/RadioImage.js
+++ src/atoms/RadioImage/RadioImage.js
@@ -1,13 +1,28 @@
+import PropTypes from 'prop-types'
 import './RadioImage.styles.css'
 
-const RadioImage = ({ alt, checked, id, group, onChange, url, value }) => {
+const RadioImage = ({ alt, checked, group, id, onChange, url, value }) => {
 
     return (
         <label htmlFor={id}>
-            <img src={url} className={`product-image-sm${checked ? ' product-image-sm-checked' : ''}`} alt={alt} />
+            <img src={url} className={`radio-image${checked ? ' radio-image-checked' : ''}`} alt={alt} />
             <input type='radio' id={id} name={group} value={value} onChange={onChange} />
         </label>
     )
+}
+
+RadioImage.propTypes = {
+    alt: PropTypes.string.isRequired,
+    checked: PropTypes.bool,
+    group: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    url: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+}
+
+RadioImage.defaultProps = {
+    checked: false,
 }
 
 export default RadioImage
