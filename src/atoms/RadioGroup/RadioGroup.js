5197e41ac73dd2b793d67dede82569150a3343ad
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 18:47:36 -0300

    style(RadioGroup): add variant horizontal and vertical

--- src/atoms/RadioGroup/RadioGroup.js
+++ src/atoms/RadioGroup/RadioGroup.js
@@ -1,23 +1,25 @@
 import PropTypes from 'prop-types'
 import './RadioGroup.styles.css'
 
-const RadioGroup = ({ children, label }) => (
+const RadioGroup = ({ children, label, variant }) => (
     <form>
-        <fieldset className="radio-group-container">
+        <fieldset className={`radio-group-container radio-group-${variant}`}>
             {Boolean(label) && (<legend>{label}</legend>)}
             {children}
         </fieldset>
-    </form>
+    </form >
 )
 
 
 RadioGroup.propTypes = {
     children: PropTypes.node.isRequired,
     label: PropTypes.oneOf([PropTypes.element, PropTypes.string]),
+    variant: PropTypes.oneOf(['horizontal', 'vertical'])
 }
 
 RadioGroup.defaultProps = {
     label: '',
+    variant: 'horizontal',
 }
 
 export default RadioGroup
