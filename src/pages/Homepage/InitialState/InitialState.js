f68a2b307c370075842795db4cfb92d68fa5006e
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 09:06:49 -0300

    feat(HomeInitialState): create component

---
+++ src/pages/Homepage/InitialState/InitialState.js
@@ -0,0 +1,10 @@
+import FeedbackWrapper from '../../templates/FeedbackWrapper'
+
+const InitialState = () => (
+    <FeedbackWrapper>
+        <h2>Nada a ser exibido</h2>
+        <p>Você ainda não procurou por nada, nos diga o que procura, por favor.</p>
+    </FeedbackWrapper>
+)
+
+export default InitialState
