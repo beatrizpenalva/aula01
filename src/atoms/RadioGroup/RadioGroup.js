18ec10b96df9d314a0a9083148aeedcd5825a793
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 16:17:32 -0300

    feat(RadioGroup): create component

---
+++ src/atoms/RadioGroup/RadioGroup.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import './RadioGroup.styles.css'
+
+const RadioGroup = ({ children, label }) => (
+    <form>
+        <fieldset className="radio-group-container">
+            {Boolean(label) && (<legend>{label}</legend>)}
+            {children}
+        </fieldset>
+    </form>
+)
+
+
+RadioGroup.propTypes = {
+    children: PropTypes.node.isRequired,
+    label: PropTypes.string,
+}
+
+RadioGroup.defaultProps = {
+    label: '',
+}
+
+export default RadioGroup
