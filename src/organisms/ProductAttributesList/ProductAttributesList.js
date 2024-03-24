08c2c5004ebc01c7381bef6aaa43f06ad8c500ca
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 17:15:32 -0300

    feat(ProductAttributesList): create component

---
+++ src/organisms/ProductAttributesList/ProductAttributesList.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import InfoRow from '../../atoms/InfoRow'
+import './ProductAttributesList.styles.css'
+
+const ProductAttributesList = ({ attributes }) => (
+    <div className='attributes-container'>
+        {attributes.map(({ id, name, value_name }, index) => {
+            const isEven = index % 2 === 0
+
+            return (
+                <InfoRow key={id} label={name} value={value_name} colored={isEven} />
+            )
+        })}
+    </div>
+)
+
+ProductAttributesList.propTypes = {
+    attributes: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        value_name: PropTypes.string.isRequired,
+    })).isRequired,
+}
+
+export default ProductAttributesList
