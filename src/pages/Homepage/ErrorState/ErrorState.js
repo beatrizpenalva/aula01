90004efe434941b253b07d22916308dd9687b3cd
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 09:06:14 -0300

    feat(HomeErrorState): create component

---
+++ src/pages/Homepage/ErrorState/ErrorState.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import Button from '../../atoms/Button'
+import FeedbackWrapper from '../../templates/FeedbackWrapper'
+
+const ErrorState = ({ onTryAgain }) => (
+    <FeedbackWrapper>
+        <h2>Ocorreu um erro</h2>
+        <p className='home-error-text'>Não conseguimos efetuar a busca. Por favor, tente novamente.</p>
+        <Button onClick={onTryAgain} variant="warning">Tentar novamente</Button>
+    </FeedbackWrapper>
+)
+
+ErrorState.propTypes = {
+    onTryAgain: PropTypes.func.isRequired,
+}
+
+export default ErrorState
