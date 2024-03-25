c64569afd3aa246358c2c2b462ce6c6e1d191cd1
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 01:25:25 -0300

    feat(PageNotFound): create component

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,7 +1,20 @@
-    const PageNotFound = () => {
-        return (
-            <h2>Pagina nao econtrada</h2>
-        )
-    }
+import { useNavigate } from 'react-router-dom'
+import Button from '../../atoms/Button'
+import FeedbackWrapper from '../../templates/FeedbackWrapper'
+import { ROUTES } from '../../utils/constants'
 
-    export default PageNotFound
+const { HOME } = ROUTES
+
+const PageNotFound = () => {
+    const navigate = useNavigate()
+
+    return (
+        <FeedbackWrapper>
+            <h1>Ops, página nao econtrada</h1>
+            <p>Para ser redirecionado à página inicial, só clicar no botão abaixo.</p>
+            <Button onClick={() => navigate(HOME)}>Ir para o início</Button>
+        </FeedbackWrapper>
+    )
+}
+
+export default PageNotFound
