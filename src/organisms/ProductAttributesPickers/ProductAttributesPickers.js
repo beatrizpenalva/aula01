19175de235bad4b3cc5cb03e124b3805216bca4f
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 21:43:01 -0300

    chore(ProductAttributesPickers): add loading state

--- src/organisms/ProductAttributesPickers/ProductAttributesPickers.js
+++ src/organisms/ProductAttributesPickers/ProductAttributesPickers.js
@@ -1,38 +1,62 @@
 import PropTypes from 'prop-types'
 import Radio from '../../atoms/Radio'
 import RadioGroup from '../../atoms/RadioGroup'
+import Skeleton from '../../atoms/Skeleton'
 import './ProductAttributesPickers.styles.css'
 
-const ProductAttributesPickers = ({ checkedPickers, onChange, pickers, productName }) => (
+const LoadingState = () => (
     <section className='product-attributes-pickers-container'>
-        <h2>Produto:</h2>
-        <h3 className='section-title'>{productName}</h3>
-        {pickers.map(({ picker_id, picker_name, products }) => {
-            const checkedOptionOnPicker = checkedPickers.find(({ category }) => category === picker_name)
-            const pickerGroupValue = checkedOptionOnPicker?.value
-            const pickerGroupTitle = pickerGroupValue ? (<p>{picker_name}: <b>{pickerGroupValue}</b></p>) : picker_name
-
+        <Skeleton height='48px' styles={{ 'margin-bottom': '12px' }} />
+        <Skeleton height='32px' styles={{ 'margin-bottom': '24px' }} />
+        {[1, 2, 3].map((item) => {
             return (
-                <RadioGroup key={picker_id} label={pickerGroupTitle}>
-                    {products.map(({ picker_label, product_id }) => {
-                        const isChecked = pickerGroupValue === picker_label
-
-                        return (
-                            <Radio
-                                checked={isChecked}
-                                group={picker_id}
-                                id={product_id}
-                                key={product_id}
-                                label={picker_label}
-                                onChange={() => onChange({ category: picker_name, value: picker_label })}
-                                value={product_id}
-                            />
-                        )
-                    })}
-                </RadioGroup>
+                <div className='product-picker-skeleton-container'>
+                    <Skeleton height='24px' />
+                    <div className='product-picker-skeleton'>
+                        <Skeleton height='32px' />
+                        <Skeleton height='32px' />
+                        <Skeleton height='32px' />
+                    </div>
+                </div>
             )
         })}
     </section>
+)
+
+const ProductAttributesPickers = ({ checkedPickers, isLoading, onChange, pickers, productName }) => (
+    <>
+        {isLoading ? <LoadingState /> : (
+            <section className='product-attributes-pickers-container'>
+                <h2>Produto:</h2>
+                <h3 className='section-title'>{productName}</h3>
+                {pickers.map(({ picker_id, picker_name, products }) => {
+                    const checkedOptionOnPicker = checkedPickers.find(({ category }) => category === picker_name)
+                    const pickerGroupValue = checkedOptionOnPicker?.value
+                    const pickerGroupTitle = pickerGroupValue ? (<p>{picker_name}: <b>{pickerGroupValue}</b></p>) : picker_name
+
+                    return (
+                        <RadioGroup key={picker_id} label={pickerGroupTitle}>
+                            {products.map(({ picker_label, product_id }) => {
+                                const isChecked = pickerGroupValue === picker_label
+
+                                return (
+                                    <Radio
+                                        checked={isChecked}
+                                        group={picker_id}
+                                        id={product_id}
+                                        key={product_id}
+                                        label={picker_label}
+                                        onChange={() => onChange({ category: picker_name, value: picker_label })}
+                                        value={product_id}
+                                    />
+                                )
+                            })}
+                        </RadioGroup>
+                    )
+                })}
+            </section>
+        )}
+    </>
 )
 
 ProductAttributesPickers.propTypes = {
@@ -40,6 +64,7 @@ ProductAttributesPickers.propTypes = {
         category: PropTypes.string.isRequired,
         value: PropTypes.string.isRequired,
     })),
+    isLoading: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
     pickers: PropTypes.arrayOf(PropTypes.shape({
         picker_id: PropTypes.string.isRequired,
@@ -54,6 +79,7 @@ ProductAttributesPickers.propTypes = {
 
 ProductAttributesPickers.defautProps = {
     checkedPickers: [],
+    isLoading: false,
 }
 
 export default ProductAttributesPickers
