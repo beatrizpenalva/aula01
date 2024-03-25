e1a04bd7e4063d058bfacedffe15f66f78a4b7c3
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 16:14:51 -0300

    test(ErrorBoundary): create stories and unit tests

---
+++ src/molecules/ErrorBoundary/ErrorBoundary.stories.js
@@ -0,0 +1,15 @@
+import ErrorBoundary from './ErrorBoundary'
+
+export default {
+    title: 'Molecules/ErrorBoundary',
+    component: ErrorBoundary,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+}
+
+export const Sample = {
+    args: {},
+}
+
