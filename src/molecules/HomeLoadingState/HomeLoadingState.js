cf5dd3455db4e59465fe422443c1802a2c2be25f
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 11:49:40 -0300

    style(HomeLoadingState): add subsection title skeleton

--- src/molecules/HomeLoadingState/HomeLoadingState.js
+++ src/molecules/HomeLoadingState/HomeLoadingState.js
@@ -3,6 +3,7 @@ import './HomeLoadingState.styles.css'
 
 const HomeLoadingState = () => (
     <main className='list-products-container-skeleton'>
+        <Skeleton height='44px' styles={{ marginBottom: '16px' }} />
         <Skeleton height='80px' />
         <Skeleton height='80px' />
         <Skeleton height='80px' />
