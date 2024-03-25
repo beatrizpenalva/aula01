d5b495ef78d755d357f602b73ed1a11e6d014a9c
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 23:03:52 -0300

    chore(ProductDetails): add loading state

--- src/organisms/ProductDetails/ProductDetails.js
+++ src/organisms/ProductDetails/ProductDetails.js
@@ -1,13 +1,32 @@
+import PropTypes from 'prop-types'
 import { useState } from 'react'
+import ListItem from '../../atoms/ListItem'
 import ProductAttributesList from '../ProductAttributesList'
 import ProductAttributesPickers from '../ProductAttributesPickers'
 import ProductImagesContainer from '../ProductImagesContainer'
+import Skeleton from '../../atoms/Skeleton'
 import './ProductDetails.styles.css'
 
-const ProductDetails = ({ details }) => {
+const FeaturesLoadingState = () => (
+    <section className='features-container'>
+        <Skeleton height='32px' />
+        <ul className='features-container-skeleton'>
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+            <Skeleton height='16px' />
+        </ul>
+    </section>
+)
+
+const ProductDetails = ({ details, isLoading }) => {
     const [checkedPickers, setCheckedPickers] = useState([])
 
-    const { attributes, main_features, name, pickers, pictures, short_description } = details
+    const { attributes, main_features, name, pickers, pictures, short_description } = details ?? {}
 
     const handleChangePicker = ({ category, value }) => {
         const pickersNotChanged = checkedPickers.filter((item) => item.category !== category)
@@ -18,22 +37,37 @@ const ProductDetails = ({ details }) => {
     return (
         <main className='product-content-container'>
             <section className='product-info-container'>
-                <ProductImagesContainer pictures={pictures} />
-                <ProductAttributesPickers checkedPickers={checkedPickers} onChange={handleChangePicker} pickers={pickers} productName={name} />
-                <section className='features-container'>
-                    <h4 className='subsection-title'>O que você precisa saber sobre este produto:</h4>
-                    {main_features.map(({ text, type }) => (<p key={type}>{text}</p>))}
-                </section>
+                <ProductImagesContainer isLoading={isLoading} pictures={pictures} />
+                <ProductAttributesPickers checkedPickers={checkedPickers} isLoading={isLoading} onChange={handleChangePicker} pickers={pickers} productName={name} />
+                {isLoading ? <FeaturesLoadingState /> : (
+                    <section className='features-container'>
+                        <h4 className='subsection-title'>O que você precisa saber sobre este produto:</h4>
+                        <ul>
+                            {main_features.map(({ text, type }) => (
+                                <ListItem key={type} showMarker>{text}</ListItem>
+                            ))}
+                        </ul>
+                    </section>
+                )}
             </section>
             <hr aria-hidden />
             <section className='description-container'>
-                <h5>Descrição:</h5>
-                <p>{short_description.content}</p>
+                {isLoading ? <Skeleton height='24px' /> : (<h5>Descrição:</h5>)}
+                {isLoading ? <Skeleton height='240px' /> : (<p>{short_description?.content}</p>)}
             </section>
             <hr aria-hidden />
-            <ProductAttributesList attributes={attributes} />
+            <ProductAttributesList attributes={attributes} isLoading={isLoading} />
         </main>
     )
+}
+
+ProductDetails.propTypes = {
+    details: PropTypes.shape({}).isRequired,
+    isLoading: PropTypes.bool,
+}
+
+ProductDetails.defaultProps = {
+    isLoading: false,
 }
 
 export default ProductDetails
