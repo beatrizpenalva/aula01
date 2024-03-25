d8bd88b61d0609e91c72ced55d43b762e28ba0a5
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 16:27:40 -0300

    fix(Product): use PageWrapper intead of Header component

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,37 +1,28 @@
 import { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import ErrorState from '../../molecules/ErrorState'
-import Header from '../../molecules/Header'
+import PageWrapper from '../../templates/PageWrapper'
 import ProductDetails from '../../organisms/ProductDetails'
 import useProductDetails from '../../hooks/useProductDetails'
-import { ROUTES } from '../../utils/constants'
-
-const { HOME } = ROUTES
 
 const ERROR_DESCRIPTION = 'Não conseguimos exibir informações sobre o produto. Por favor, tente novamente.'
 
 const Product = () => {
-    const navigate = useNavigate()
     const { productId } = useParams()
     const { details, getProductDetails, isError, isLoading } = useProductDetails({ productId })
-
-    const handleSearch = value => {
-        navigate(HOME, { state: { search: value } })
-    }
 
     useEffect(() => {
         getProductDetails()
     }, [productId])
 
     return (
-        <>
-            <Header onSearch={value => handleSearch(value)} />
+        <PageWrapper>
             {isError ?
                 <ErrorState description={ERROR_DESCRIPTION} onTryAgain={getProductDetails} />
                 :
                 <ProductDetails details={details} isLoading={isLoading} />
             }
-        </>
+        </PageWrapper>
     )
 }
 
