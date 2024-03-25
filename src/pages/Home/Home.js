07f2b7dce3b72fcbacc636482180e3d40667f574
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 02:07:19 -0300

    refactor(Home): control only app initial state

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,52 +1,15 @@
-import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import ErrorState from '../../molecules/ErrorState'
-import Header from '../../molecules/Header'
-import HomeEmptyState from '../../molecules/HomeEmptyState'
-import HomeInitialState from '../../molecules/HomeInitialState'
-import HomeLoadingState from '../../molecules/HomeLoadingState'
-import HomeSearchResultList from '../../organisms/HomeSearchResultList'
-import useSearchProducts from '../../hooks/useSearchProducts'
-
-const ERROR_DESCRIPTION = 'Não conseguimos efetuar a busca. Por favor, tente novamente.'
+import FeedbackWrapper from '../../templates/FeedbackWrapper'
+import PageWrapper from '../../templates/PageWrapper'
 
 const Home = () => {
-    const location = useLocation()
-    const { state: { search } } = location
-
-    const [product, setProduct] = useState(search ?? '')
-
-    const {
-        getProductsAvailable,
-        isEmpty,
-        isError,
-        isLoading,
-        productsList,
-        renderContent,
-        renderInitialState
-    } = useSearchProducts({ product })
-
-    const handleSubmitProduct = (value) => {
-        setProduct(value)
-        getProductsAvailable()
-    }
-
-    useEffect(() => {
-        getProductsAvailable()
-    }, [product])
 
     return (
-        <>
-            <Header onSearch={handleSubmitProduct} />
-            {isLoading ? (<HomeLoadingState />) : (
-                <>
-                    {isError && (<ErrorState description={ERROR_DESCRIPTION} onTryAgain={getProductsAvailable} />)}
-                    {renderInitialState && (<HomeInitialState />)}
-                    {isEmpty && (<HomeEmptyState product={product} />)}
-                    {renderContent && (<HomeSearchResultList product={product} productsList={productsList} />)}
-                </>
-            )}
-        </>
+        <PageWrapper >
+            <FeedbackWrapper>
+                <h2>Nada a ser exibido</h2>
+                <p>Você ainda não procurou por nada, nos diga o que procura, por favor.</p>
+            </FeedbackWrapper>
+        </PageWrapper>
     )
 }
 
