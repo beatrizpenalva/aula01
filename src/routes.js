ffa27306e0235ebf92ff7998cd3e9cd5e33360de
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 02:36:49 -0300

    refactor: get product search from url

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,21 @@ import ErrorBoundary from './molecules/ErrorBoundary'
 import Home from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
 import Product from './pages/Product'
+import SearchResult from './pages/SearchResult'
 import { ROUTES } from './utils/constants'
 
-const { HOME, PRODUCT } = ROUTES
+const { HOME, PRODUCT, SEARCH_RESULT } = ROUTES
 
 const router = createBrowserRouter(
   [
     {
       path: HOME,
       element: <Home />,
+      errorElement: <ErrorBoundary />,
+    },
+    {
+      path: SEARCH_RESULT,
+      element: <SearchResult />,
       errorElement: <ErrorBoundary />,
     },
     {
