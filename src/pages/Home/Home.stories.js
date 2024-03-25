2d55d3c2dc915d4d63afe1fef07fccf71e5c697f
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 16:33:20 -0300

    doc(Home): create stories

---
+++ src/pages/Home/Home.stories.js
@@ -0,0 +1,14 @@
+import Home from './Home'
+
+export default {
+    title: 'Pages/Home',
+    component: Home,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+}
+
+export const Sample = {
+    args: {},
+}
