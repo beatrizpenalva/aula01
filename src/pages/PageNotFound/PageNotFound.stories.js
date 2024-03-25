dab31b4e54ab72becbd84dd5cd5cd496aca2e97a
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 16:31:20 -0300

    test(PageNotFound): create stories and unit tests

---
+++ src/pages/PageNotFound/PageNotFound.stories.js
@@ -0,0 +1,14 @@
+import PageNotFound from './PageNotFound'
+
+export default {
+    title: 'Pages/PageNotFound',
+    component: PageNotFound,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+}
+
+export const Sample = {
+    args: {},
+}
---
+++ src/pages/PageNotFound/PageNotFound.test.js
@@ -0,0 +1,26 @@
+import userEvent from '@testing-library/user-event'
+import { useNavigate } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import { composeStory } from '@storybook/react'
+import Meta, { Sample } from './PageNotFound.stories'
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(() => ({
+        navigate: jest.fn(),
+    }))
+}))
+
+describe('PageNotFound', () => {
+    test('redirects to home page when click on go back button', () => {
+        const navigate = jest.fn()
+        useNavigate.mockImplementation(() => navigate)
+
+        const SampleWrapper = composeStory(Sample, Meta)
+        render(<SampleWrapper />)
+
+        userEvent.click(screen.getByRole('button', { name: 'Ir para o início' }))
+
+        expect(navigate).toHaveBeenCalledWith('/')
+    })
+})
