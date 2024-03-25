6a8fe31a2931d4e64a453aea2e80ab145ecee7c1
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 11:49:01 -0300

    refactor(CardProduct): add default image

--- src/molecules/CardProduct/CardProduct.js
+++ src/molecules/CardProduct/CardProduct.js
@@ -1,17 +1,27 @@
 import PropTypes from 'prop-types'
+import NoPicturesPlaceholder from '../../atoms/NoPicturesPlaceholder'
 import './CardProduct.styles.css'
 
-const CardProduct = ({ onClick, product }) => (
-    <div type="button" onClick={onClick} className="card-container">
-        <div className='card-image-wrapper'>
-            <img src={product.pictures[0].url} aria-hidden className="card-image" />
+const CardProduct = ({ onClick, product }) => {
+    const { domain_id, name, pictures } = product ?? {}
+    const hasPictures = pictures.length
+
+    return (
+        <div type="button" onClick={onClick} className="card-container">
+            {hasPictures ? (
+                <div className='card-image-wrapper'>
+                    <img src={product.pictures[0].url} aria-hidden className="card-image" />
+                </div>
+            ) : (
+                <NoPicturesPlaceholder width='64px' />
+            )}
+            <div className="card-text-container">
+                <p>{name}</p>
+                <p>Por: {domain_id}</p>
+            </div>
         </div>
-        <div className="card-text-container">
-            <p>{product.name}</p>
-            <p>Por: {product.domain_id}</p>
-        </div>
-    </div>
-)
+    )
+}
 
 CardProduct.propTypes = {
     onClick: PropTypes.func.isRequired,
