a0b0e9d393dd80fc91e1560be8e9b0b4fa850b31
beatriz-penalva <biapenalva@gmail.com>
2024-03-24 21:21:36 -0300

    chore(ProductAttributesList): add loading state

--- src/organisms/ProductAttributesList/ProductAttributesList.js
+++ src/organisms/ProductAttributesList/ProductAttributesList.js
@@ -1,16 +1,30 @@
 import PropTypes from 'prop-types'
 import InfoRow from '../../atoms/InfoRow'
+import Skeleton from '../../atoms/Skeleton'
 import './ProductAttributesList.styles.css'
 
-const ProductAttributesList = ({ attributes }) => (
-    <section className='attributes-container'>
-        {attributes.map(({ id, name, value_name }, index) => {
-            const isEven = index % 2 === 0
-
-            return (
-                <InfoRow key={id} label={name} value={value_name} colored={isEven} />
-            )
+const LoadingState = () => {
+    <div className='attributes-skeleton-container'>
+        {[1, 2, 3, 4, 5, 6].map((item) => {
+            return <Skeleton key={item} height='32px' />
         })}
+    </div>
+}
+
+const ProductAttributesList = ({ attributes, isLoading }) => (
+    <section className='attributes-container'>
+        {isLoading ? <LoadingState />
+            : (
+                <>
+                    {attributes.map(({ id, name, value_name }, index) => {
+                        const isEven = index % 2 === 0
+
+                        return (
+                            <InfoRow key={id} label={name} value={value_name} colored={isEven} />
+                        )
+                    })}
+                </>
+            )}
     </section>
 )
 
@@ -20,6 +34,11 @@ ProductAttributesList.propTypes = {
         name: PropTypes.string.isRequired,
         value_name: PropTypes.string.isRequired,
     })).isRequired,
+    isLoading: PropTypes.bool,
+}
+
+ProductAttributesList.defaultProps = {
+    isLoading: false,
 }
 
 export default ProductAttributesList
