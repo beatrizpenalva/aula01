7567d16801d06500be5ff9d435ffb5d20bedbd7b
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 23:05:39 -0300

    fix(SearchResult): router memory on stories file

---
+++ src/pages/SearchResult/SearchResult.stories.js
@@ -0,0 +1,29 @@
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import SearchResult from './SearchResult'
+
+const routes = [
+    {
+        path: '/result/:searchId',
+        element: <SearchResult />,
+    },
+];
+
+const router = createMemoryRouter(routes, {
+    initialEntries: ['/result/423423423'],
+    initialIndex: 1,
+})
+
+const BaseComponent = () => <RouterProvider router={router} />
+
+export default {
+    title: 'Pages/SearchResult',
+    component: BaseComponent,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+}
+
+export const Sample = {
+    args: {},
+}
---
+++ src/pages/SearchResult/SearchResult.test.js
@@ -0,0 +1,7 @@
+describe('SearchResult', () => {
+    test.todo('calls get products available function when search id value change')
+    test.todo('rendering loading state')
+    test.todo('rendering empty state')
+    test.todo('rendering error state')
+    test.todo('rendering content')
+})
