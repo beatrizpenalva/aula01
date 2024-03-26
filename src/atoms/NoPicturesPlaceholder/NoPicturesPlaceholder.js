872e174e0927b187d5fd1b648408c5fd2fcf926e
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 22:41:15 -0300

    refactor: some adjusments

--- src/atoms/NoPicturesPlaceholder/NoPicturesPlaceholder.js
+++ src/atoms/NoPicturesPlaceholder/NoPicturesPlaceholder.js
@@ -3,7 +3,11 @@ import IconNoPictures from '../../assets/IconNoPictures.png'
 import './NoPicturesPlaceholder.styles.css'
 
 const NoPicturesPlaceholder = ({ border, size }) => (
-    <div className={`no-pictures-container ${border ? 'no-pictures-container-with-border' : ''}`} style={{ height: size, width: size }} data-testid='default-img-container'>
+    <div
+        className={`no-pictures-container ${border ? 'no-pictures-container-with-border' : ''}`}
+        style={{ height: size, width: size }}
+        data-testid='default-img-container'
+    >
         <img src={IconNoPictures} aria-hidden className='no-pictures-icon' />
     </div>
 )
