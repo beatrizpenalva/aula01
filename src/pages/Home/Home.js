9b3a382bc7b571bb9ac17a27ea2efddc4d7ee3b2
beatriz-penalva <biapenalva@gmail.com>
2024-03-25 23:04:37 -0300

    fix(Home): router memory on stories file

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,13 @@
 import FeedbackWrapper from '../../templates/FeedbackWrapper'
 import PageWrapper from '../../templates/PageWrapper'
 
-const Home = () => {
-
-    return (
-        <PageWrapper >
-            <FeedbackWrapper>
-                <h2>Nada a ser exibido</h2>
-                <p>Você ainda não procurou por nada, nos diga o que procura, por favor.</p>
-            </FeedbackWrapper>
-        </PageWrapper>
-    )
-}
+const Home = () => (
+    <PageWrapper >
+        <FeedbackWrapper>
+            <h2>Nada a ser exibido</h2>
+            <p>Você ainda não procurou por nada, nos diga o que procura, por favor.</p>
+        </FeedbackWrapper>
+    </PageWrapper>
+)
 
 export default Home
--- src/pages/Home/Home.stories.js
+++ src/pages/Home/Home.stories.js
@@ -1,8 +1,23 @@
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import Home from './Home'
+
+const routes = [
+    {
+        path: '/',
+        element: <Home />,
+    },
+];
+
+const router = createMemoryRouter(routes, {
+    initialEntries: ['/'],
+    initialIndex: 1,
+})
+
+const BaseComponent = () => <RouterProvider router={router} />
 
 export default {
     title: 'Pages/Home',
-    component: Home,
+    component: BaseComponent,
     tags: ['autodocs'],
     parameters: {
         layout: 'fullscreen',
