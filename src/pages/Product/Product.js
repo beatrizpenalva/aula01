7348396dc89092cd3a2d105ee92ef21bd75a3086
beatriz-penalva <biapenalva@gmail.com>
2024-03-29 21:49:57 -0300

    fix(ProductDetails): get product details with category id

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,14 +1,32 @@
+import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import ErrorState from '../../molecules/ErrorState'
 import PageWrapper from '../../templates/PageWrapper'
 import ProductDetails from '../../organisms/ProductDetails'
+import useProductDetails from '../../hooks/useProductDetails'
+
+const ERROR_DESCRIPTION = 'Não conseguimos exibir informações sobre o produto. Por favor, tente novamente.'
 
 const Product = () => {
     const location = useLocation()
     const { state: { item } } = location
 
+    const { details, getProductDetails, isError, isLoading } = useProductDetails({ productId: item.catalog_product_id })
+
+    console.log(details)
+
+    useEffect(() => {
+        getProductDetails()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <PageWrapper>
-            <ProductDetails details={item} />
+            {isError ?
+                <ErrorState description={ERROR_DESCRIPTION} onTryAgain={getProductDetails} />
+                :
+                <ProductDetails details={item} isLoading={isLoading} />
+            }
         </PageWrapper>
     )
 }
