54fea82940208ce3b3c03ff2b3b7ea2dd1f26d57
beatriz-penalva <biapenalva@gmail.com>
2024-03-30 10:36:05 -0300

    fix(routes): remove unused component imported

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createBrowserRouter, redirect } from 'react-router-dom'
 import ErrorBoundary from './molecules/ErrorBoundary'
-import Home from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
 import Product from './pages/Product'
 import SearchResult from './pages/SearchResult'
