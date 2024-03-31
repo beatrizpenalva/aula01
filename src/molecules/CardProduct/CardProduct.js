29e710155c7aa7f7f40196c934ca56356416d8e7
beatriz-penalva <biapenalva@gmail.com>
2024-03-30 22:53:17 -0300

    style: adjustments to make layout responsive

--- src/molecules/CardProduct/CardProduct.js
+++ src/molecules/CardProduct/CardProduct.js
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types'
 import NoPicturesPlaceholder from '../../atoms/NoPicturesPlaceholder'
+import Stamp from '../../atoms/Stamp'
 import './CardProduct.styles.css'
+import { formatCurrency } from '../../utils/helpers'
 
+// TODO: Adjust style if has not sale price
 const CardProduct = ({ onClick, product }) => {
-    const { domain_id, thumbnail, title } = product ?? {}
+    const {
+        official_store_name,
+        original_price,
+        price,
+        seller: { nickname },
+        shipping: { free_shipping },
+        thumbnail,
+        title
+    } = product ?? {}
 
     return (
         <button
@@ -24,9 +35,14 @@ const CardProduct = ({ onClick, product }) => {
             ) : (
                 <NoPicturesPlaceholder size='64px' />
             )}
-            <div className="card-text-container">
+            <div className="card-second-column">
                 <p>{title}</p>
-                <p>Por: {domain_id}</p>
+                <p>Por {official_store_name || nickname}</p>
+                <div className='card-price-line'>
+                    <p><b>{formatCurrency(price)}</b></p>
+                    <p>{formatCurrency(original_price)}</p>
+                </div>
+                {free_shipping && <Stamp text='Frete grátis' />}
             </div>
         </button>
     )
@@ -35,11 +51,17 @@ const CardProduct = ({ onClick, product }) => {
 CardProduct.propTypes = {
     onClick: PropTypes.func.isRequired,
     product: PropTypes.shape({
-        domain_id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        pictures: PropTypes.arrayOf(PropTypes.shape({
-            url: PropTypes.string,
-        })).isRequired,
+        official_store_name: PropTypes.string.isRequired,
+        original_price: PropTypes.string.isRequired,
+        price: PropTypes.string,
+        seller: PropTypes.shape({
+            nickname: PropTypes.string,
+        }),
+        shipping: PropTypes.shape({
+            free_shipping: PropTypes.bool.isRequired
+        }).isRequired,
+        thumbnail: PropTypes.string,
+        title: PropTypes.string.isRequired,
     }).isRequired,
 }
 
